ca11117af4cdb76bb972181ea42d5719f78151cc
salonisingh-23 <salonisingh5723@gmail.com>
2021-10-09 10:59:41 +0530

    tut1

---
+++ tut1.py
@@ -0,0 +1,24 @@
+#list
+shopping=["bread","curd","fruits"]
+print(shopping)
+
+for items in shopping:
+    print(items)
+
+for items in range(3):
+    print(shopping[items],end=" /") #end removes next line
+
+#Append
+shopping.append("shampoo")
+print(shopping)
+
+#count
+ages=[12,22,23,22,4,7,16,14,5]
+print(ages.count(22))
+print(ages.count(3))
+
+#len
+print(len(ages))
+
+#insert
+print(ages.count(22))
